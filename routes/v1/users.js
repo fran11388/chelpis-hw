06c84b8aeb45b16f3b7777e5ff9d47923b6227c0
fran11388 <fran11388@gmail.com>
2020-12-26 22:50:16 +0800

    update

--- routes/v1/users.js
+++ routes/v1/users.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const router = express.Router();
-const userController = require('../../controllers/user');
+const UserController = require('../../controllers/user');
 const Auth = require('../../middlewares/auth');
 const UserValidate = require('../../middlewares/validate/user');
 
-router.post('/register', UserValidate.register, userController.register);
-router.post('/login', UserValidate.login, userController.login);
-router.get('/me', Auth.login, userController.me);
-router.get('/', Auth.login, userController.getUsers);
-router.get('/:userId', Auth.login, userController.getUser);
-router.patch('/:userId/score', Auth.login, Auth.isTeacher, userController.updateScore);
-router.post('/logout', userController.logout);
+router.post('/register', UserValidate.register, UserController.register);
+router.post('/login', UserValidate.login, UserController.login);
+router.get('/me', Auth.login, UserController.me);
+router.get('/', Auth.login, UserController.getUsers);
+router.get('/:userId', Auth.login, UserController.getUser);
+router.patch('/:userId/score', Auth.login, Auth.isTeacher, UserController.updateScore);
+router.post('/logout', UserController.logout);
 
 module.exports = router;
