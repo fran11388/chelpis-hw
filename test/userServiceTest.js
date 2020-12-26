f65647c139e954ab50f27312712f071308bc5c6a
fran11388 <fran11388@gmail.com>
2020-12-26 17:24:28 +0800

    add testing

---
+++ test/userServiceTest.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const assert = require("chai").assert;
+const mocha = require("mocha");
+
+const UserService = require('../services/user');
+const UserModel = require('../models/user');
+const {ROLE} = require('../constant/index');
+
+//todo mock data access layer
+
+// const mockUserModel={
+//     usersObj:{},
+// };
+// mockUserModel.findAll=async(options)=>{
+//
+// }
+const userServiceInstance = new UserService({UserModel});
+
+
+mocha.describe("User Service", () => {
+
+    mocha.describe("User register", () => {
+        it("User can register", async () => {
+            let email = 'fran113885@gmail.com'
+            let password = 'pass';
+            let name = 'frank1235';
+            let role = ROLE.STUDENT;
+
+            let registerResult = await userServiceInstance.registerUser({name, email, password, role});
+            assert(registerResult.message === undefined, 'register fail');
+
+        });
+
+    });
+
+    mocha.describe("User login", () => {
+        it("User can login when email and password correct", async () => {
+            let email = 'fran113884@gmail.com'
+            let password = 'pass';
+            let loginResult = await userServiceInstance.login({email, password});
+            assert(loginResult.message === undefined, 'login fail');
+
+        });
+
+    });
+
+});
